004a396ac5fe31aab21692ef926136c0449c6b5b
ISZenzi <zenzirovi@gmail.com>
2025-11-07 20:58:52 +0300

    Update requirements

--- src/main.py
+++ src/main.py
@@ -8,52 +8,50 @@ from src.get_grep import grep
 
 
 def main():
-    while True:
-        current_dir = os.getcwd()
-        user_input = input(f"{current_dir}$ ")
-        if not user_input.strip():
-            continue
-        args = user_input.split()
-        command = args[0]
-        argument = " ".join(args[1:])
-        if command == "ls":
-            ls(argument)
-        elif command == "cd":
-            cd(argument)
-        elif command == "cat":
-            cat(argument)
-        elif command == "cp":
-            cp(argument)
-        elif command == "mv":
-            mv(argument)
-        elif command == "rm":
-            rm(argument)
-        elif command == "zip":
-            zip(argument)
-        elif command == "unzip":
-            unzip(argument)
-        elif command == "tar":
-            tar(argument)
-        elif command == "untar":
-            untar(argument)
-        elif command == "grep":
-            grep(argument)
-        elif command == "exit":
-            break
-        else:
-            e = f"Неизвестная команда '{command}'"
-            logging.error(f"ERROR: {e}")
-            print(f"ERROR: {e}")
-            continue
+    try:
+        while True:
+            current_dir = os.getcwd()
+            user_input = input(f"{current_dir}$ ")
+            if not user_input.strip():
+                continue
+            args = user_input.split()
+            command = args[0]
+            argument = " ".join(args[1:])
+            if command == "ls":
+                ls(argument)
+            elif command == "cd":
+                cd(argument)
+            elif command == "cat":
+                cat(argument)
+            elif command == "cp":
+                cp(argument)
+            elif command == "mv":
+                mv(argument)
+            elif command == "rm":
+                rm(argument)
+            elif command == "zip":
+                zip(argument)
+            elif command == "unzip":
+                unzip(argument)
+            elif command == "tar":
+                tar(argument)
+            elif command == "untar":
+                untar(argument)
+            elif command == "grep":
+                grep(argument)
+            elif command == "exit":
+                break
+            else:
+                e = f"Неизвестная команда '{command}'"
+                logging.error(f"ERROR: {e}")
+                print(f"ERROR: {e}")
+                continue
+    except KeyboardInterrupt as e:
+        logging.error(f"ERROR: {e}")
+        print(f"ERROR: {e}")
 
 
 if __name__ == "__main__":
     setup_logging()
     main()
-
-"""
-нужно ли разбираться с правами доступа? в команде cp нужно проверять права доступа? в команде ls выводить права доступа
-как понять какие ошибки можно объединить в один класс?
-
-
-"""
+"""сделать тесты из комбинации команд"""
